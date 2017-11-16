4c2eb95c0ac4ef6a63abdcf29820424a13f2f359
Marco <marco1@live.de>
2017-11-16 22:18:09 +0100

    Create new subclass 'FileUpload' that extends 'Upload'

---
+++ src/FileUpload.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * PHP-FileUpload (https://github.com/delight-im/PHP-FileUpload)
+ * Copyright (c) delight.im (https://www.delight.im/)
+ * Licensed under the MIT License (https://opensource.org/licenses/MIT)
+ */
+
+namespace Delight\FileUpload;
+
+/** Helper for simple and convenient file uploads */
+final class FileUpload extends Upload {}
